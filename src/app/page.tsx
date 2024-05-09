1b43a63848a49f8a608c428d8e37d06bc0c59bd0
Steve Ruiz <steveruizok@gmail.com>
2024-05-09 15:48:59 +0100

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,18 @@ import styles from './page.module.css'
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div className={styles.center}>
+			<div
+				style={{
+					position: 'absolute',
+					inset: 0,
+					display: 'flex',
+					alignItems: 'start',
+					justifyContent: 'center',
+					flexDirection: 'column',
+					padding: 16,
+				}}
+			>
+				<h1>AIBFWG</h1>
 				<p>ai boyfriend working group</p>
 			</div>
 		</main>
