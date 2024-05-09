49e835d05386c901aeff4f09554a53cada5e52d9
Steve Ruiz <steveruizok@gmail.com>
2024-05-09 15:46:42 +0100

    push

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Home() {
+	return (
+		<main className={styles.main}>
+			<div className={styles.center}>
+				<p>ai boyfriend working group</p>
+			</div>
+		</main>
+	)
+}
